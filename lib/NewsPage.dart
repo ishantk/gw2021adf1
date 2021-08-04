45691d89e68747f02a61e4af0d196c533a2cf02d
admin <er.ishant@gmail.com>
2021-08-04 16:56:37 +0530

    JSON Parsing in Flutter

--- lib/NewsPage.dart
+++ lib/NewsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gw2021adf1/WebViewPage.dart';
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
@@ -13,6 +14,41 @@ Future<String> fetchNews() async{
 
 class NewsPage extends StatelessWidget {
 
+  parseNews(String response, BuildContext context){
+    var mapAsData = convert.jsonDecode(response);
+    List articles = mapAsData['articles'];
+    //return Text("Total Articles: ${articles.length}");
+
+    List<Widget> widgets = [];
+
+    articles.forEach((element) {
+      widgets.add(
+          element["urlToImage"] != null ? Image.network(element["urlToImage"]) : Container()
+      );
+      widgets.add(
+         Divider()
+      );
+      widgets.add(
+          ListTile(
+            title: Text(element["title"]),
+            subtitle: Text(element["publishedAt"]),
+            onTap: (){
+              // Explicit Navigation: Navigating from One UI to Another
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => WebViewPage(url: element['url']),)
+              );
+
+            },
+          )
+      );
+    });
+
+    return ListView(
+      padding: EdgeInsets.all(8),
+      children: widgets,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -20,13 +56,13 @@ class NewsPage extends StatelessWidget {
      future: fetchNews(),
       builder: (context, snapshot) {
         if(snapshot.hasData){
-          return Text(snapshot.data.toString());
+          //return Text(snapshot.data.toString());
+          return parseNews(snapshot.data.toString(), context);
         }else{
           return Center(
-            child: CircularProgressIndicator()
+              child: CircularProgressIndicator()
           );
         }
-        return Container();
       },
 
     );
