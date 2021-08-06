58f20709ebe3102b7389a98dd20503be60c81adf
admin <er.ishant@gmail.com>
2021-08-06 17:07:43 +0530

    BottomNavigationBar

--- lib/home/home-page.dart
+++ lib/home/home-page.dart
@@ -8,12 +8,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  int index = 0;
+
+  List<Widget> widgets = [
+    Center(child: Text("HOME PAGE")),
+    Center(child: Text("SEARCH PAGE")),
+    Center(child: Text("PROFILE PAGE")),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Welcome to Foodie"),
+
+      appBar: AppBar(
+        title: Text("Foodie"),
+        actions: [
+          IconButton(
+              onPressed: (){
+
+          }, icon: Icon(Icons.shopping_cart),
+            tooltip: "Shopping Cart",
+
+          )
+        ],
       ),
+
+      body: widgets[index],
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          // 0
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "HOME"
+          ),
+          // 1
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "SEARCH"
+          ),
+          //2
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_box),
+              label: "PROFILE"
+          )
+        ],
+
+        currentIndex: index,
+        selectedFontSize: 16,
+        selectedItemColor: Colors.deepOrange,
+        onTap: (idx){ // idx will have value of the index of BottomNavBarItem
+          setState(() {
+            index = idx;
+          });
+        },
+      ),
+
     );
   }
 }
